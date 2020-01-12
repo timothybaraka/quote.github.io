49807b9af739e7594f72a3b075469d65de06c0aa
timothybaraka <timothybaraka44@gmail.com>
2020-01-12 12:30:25 +0300

    Add the first quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: '/app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quote';
+  quotes:string[];
+
+  
+  constructor(){
+    this.quotes=['show me how you drive i tell you who you are']
+  }
 }
