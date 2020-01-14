8a8948dedc11867ae13de25283f6c6b8606a7d2c
timothybaraka <timothybaraka44@gmail.com>
2020-01-14 10:52:34 +0300

    Add logic to quote-count.ts

---
+++ src/app/quote-count.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { QuoteCountPipe } from './quote-count.pipe';
+
+describe('QuoteCountPipe', () => {
+  it('create an instance', () => {
+    const pipe = new QuoteCountPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
